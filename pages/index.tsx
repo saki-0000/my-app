7ea3d05e8cb469c0af46562f22d16dcfc6912ef5
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-10-18 01:08:41 +0900

    チュートリアル分もプッシュ

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,52 +2,126 @@ import type { NextPage } from "next";
 import fs from "fs";
 import matter from "gray-matter";
 import PostCard from "../components/PostCard";
+import { Authenticator } from "@aws-amplify/ui-react";
+import {
+  Amplify,
+  API,
+  Auth,
+  withSSRContext,
+} from "aws-amplify";
+import Head from "next/head";
+import awsExports from "../src/aws-exports";
+import { createPost } from "../src/graphql/mutations";
+import { listPosts } from "../src/graphql/queries";
+import styles from "../styles/Home.module.css";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+export async function getServerSideProps({ req }) {
+  const SSR = withSSRContext({ req });
+  const response = await SSR.API.graphql({
+    query: listPosts,
+  });
+
+  return {
+    props: {
+      posts: response.data.listPosts.items,
+    },
+  };
+}
+
+async function handleCreatePost(event) {
+  event.preventDefault();
+
+  const form = new FormData(event.target);
+
+  try {
+    const { data } = await API.graphql({
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+      query: createPost,
+      variables: {
+        input: {
+          title: form.get("title"),
+          content: form.get("content"),
+        },
+      },
+    });
+
+    window.location.href = `/posts/${data.createPost.id}`;
+  } catch ({ errors }) {
+    console.error(...errors);
+    throw new Error(errors[0].message);
+  }
+}
 
 type Post = {
   slug: string;
-  frontMatter: any;
+  frontMatter: Object;
 };
 
-interface Props {
-  posts: Post[];
-}
-
-export const getStaticProps = () => {
-  const files = fs.readdirSync("posts");
-  const posts: Post[] = files.map((fileName) => {
-    const slug = fileName.replace(/\.md$/, "");
-    const fileContent = fs.readFileSync(
-      `posts/${fileName}`,
-      "utf-8"
-    );
-    const { data } = matter(fileContent);
-    return {
-      frontMatter: data,
-      slug,
-    };
-  });
-  const sortedPosts = posts.sort((postA, postB) =>
-    new Date(postA.frontMatter.date) >
-    new Date(postB.frontMatter.date)
-      ? -1
-      : 1
-  );
-  return {
-    props: {
-      posts: sortedPosts,
-    },
-  };
-};
-
-const Home: NextPage<Props> = ({ posts }) => {
+const Home: NextPage = ({ posts = [] }) => {
   return (
-    <div className="my-8">
-      <h1>ユーザー名の記事一覧</h1>
-      <div className="grid grid-cols-3 gap-4">
-        {posts.map((post) => (
-          <PostCard key={post.slug} post={post} />
-        ))}
-      </div>
+    <div className={styles.container}>
+      <Head>
+        <title>Amplify + Next.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Amplify + Next.js</h1>
+
+        <p className={styles.description}>
+          <code className={styles.code}>
+            {posts.length}
+          </code>
+          posts
+        </p>
+
+        <div className={styles.grid}>
+          {posts.map((post) => (
+            <a
+              className={styles.card}
+              href={`/posts/${post.id}`}
+              key={post.id}
+            >
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+            </a>
+          ))}
+
+          <div className={styles.card}>
+            <h3 className={styles.title}>New Post</h3>
+
+            <Authenticator>
+              <form onSubmit={handleCreatePost}>
+                <fieldset>
+                  <legend>Title</legend>
+                  <input
+                    defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
+                    name="title"
+                  />
+                </fieldset>
+
+                <fieldset>
+                  <legend>Content</legend>
+                  <textarea
+                    defaultValue="I built an Amplify app with Next.js!"
+                    name="content"
+                  />
+                </fieldset>
+
+                <button>Create Post</button>
+                <button
+                  type="button"
+                  onClick={() => Auth.signOut()}
+                >
+                  Sign out
+                </button>
+              </form>
+            </Authenticator>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
